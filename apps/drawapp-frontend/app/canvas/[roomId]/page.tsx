00e16d6ecb37149bb46a89196a26fe90cb9211bd
veerbhan <veerbhansingh70@gmail.com>
2025-07-15 01:22:37 +0530

    restructure

--- apps/drawapp-frontend/app/canvas/[roomId]/page.tsx
+++ apps/drawapp-frontend/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { initDraw } from "@/app/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas(){
@@ -6,43 +7,13 @@ export default function Canvas(){
 
     useEffect(()=>{
         if(canvasRef.current){
-            const canvas = canvasRef.current ;
-            const ctx = canvas.getContext("2d");
-
-            if(!ctx){
-                return ;
-            }
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-
-            canvas.addEventListener("mousedown",(e)=>{
-                clicked = true;
-                startX = e.clientX;
-                startY = e.clientY;
-            })
-
-            canvas.addEventListener("mouseup",(e)=>{
-                clicked = false;
-                console.log(e.clientX);
-                console.log(e.clientY);
-            })
-           
-
-            canvas.addEventListener("mousemove",(e) => {
-                if(clicked){
-                    const width = e.clientX - startX;
-                    const height = e.clientY - startY;
-                    ctx.clearRect(0, 0, canvas.width, canvas.height); // first clear rectangle it
-                    ctx.strokeRect(startX, startY, width, height)   // then strole it
-                }
-            })
+            initDraw(canvasRef.current)
         }
     },[canvasRef])
 
     return <div>
     
-        <canvas ref={canvasRef} width={500} height={500} >
+        <canvas ref={canvasRef} width={2000} height={1000} >
             
         </canvas>
     </div>
